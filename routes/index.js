da2dac2a455065b5cd0403ffd99262f849f07035
xiaowao <1668856072@qq.com>
2019-03-14 12:03:51 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+router.post('/login', async (ctx, next) => {
+  console.log(ctx)
+  ctx.body = {
+    status: 1,
+    data: 'connect ready'
+  }
+})
+
+module.exports = router
